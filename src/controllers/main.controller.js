3e77361d906e41ce63b5a2f9488a8e4dda69c4a3
yisusvg <yisusvg.1@gmail.com>
2024-01-03 19:06:56 -0300

    empezando de cero

--- src/controllers/main.controller.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    home: (req, res) => res.render('home', {
-        title: "Home - Funkoshop"
-    }),
-    contact: (req, res) => res.send('esta es la vista de contacto'),
-    faqs: (req, res) => res.send('esta es la vista de Preguntas frecuentes')
-}
